e64e155ab18a984c2f7d3ccd0f18dc319f9c409e
rnsilva <rnsilva@fazenda.ms.gov.br>
2025-07-28 08:05:41 -0400

    Infrastructure, Data, docker - Implementações

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Infrastructure.Data;
+﻿using Infrastructure.Config;
+
+namespace Infrastructure.Data;
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
 }

88f4ab14dd0f80b619abcf219f2b8836876d6af5
rnsilva <rnsilva@fazenda.ms.gov.br>
2025-08-20 10:40:43 -0400

    Ajuste erro faltando parametro

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        var spec = new ProductSpecification(brand, type);
+        var spec = new ProductSpecification(brand, type, sort);
 
         var products = await repo.ListAsync(spec);
 

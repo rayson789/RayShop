aa2f39b4b63aae5167415a7e2c144b749ca05adf
rnsilva <rnsilva@fazenda.ms.gov.br>
2025-09-23 10:35:53 -0400

    Types

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  baseUrl = 'http://localhost:5000/api/';
+  private http = inject(HttpClient);
+  title = 'RayShop';
+  products: Product [] = []
 
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
